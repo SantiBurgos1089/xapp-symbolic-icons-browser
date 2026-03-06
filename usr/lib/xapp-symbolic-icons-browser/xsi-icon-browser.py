3ced4fbae62e7837cc33eabb44896f98566d5365
Santiago Burgos <santiago.burgos1089@gmail.com>
2026-03-06 13:36:13 -0600

    added main program with the required controls in order to show the information needed

--- usr/lib/xapp-symbolic-icons-browser/xsi-icon-browser.py
+++ usr/lib/xapp-symbolic-icons-browser/xsi-icon-browser.py
@@ -6,9 +6,12 @@ from gi.repository import Gtk
 
 gi.require_version('Adap', '1')
 from gi.repository import Adap as Adw
+from find_icons import find_xsi_icons
 
-# Application ID
+# Application ID and variables
 app_id = "xyz.agatinos.xsi-icon-browser"
+icon_dir = "/usr/share/icons"
+search_pattern = "xsi-*"
 
 class MainWindow(Adw.ApplicationWindow):
     def __init__(self, *args, **kwargs):
@@ -27,8 +30,38 @@ class MainWindow(Adw.ApplicationWindow):
         app_headerbar = Adw.HeaderBar()
         app_headerbar.set_show_title(True)
 
+        scroll_control = Gtk.ScrolledWindow()
+        scroll_control.set_policy(Gtk.PolicyType.AUTOMATIC, Gtk.PolicyType.AUTOMATIC)
+        scroll_control.set_vexpand(True)
+        
+        content_listbox = Gtk.ListBox()
+        content_listbox.set_selection_mode(Gtk.SelectionMode.NONE)
+        content_listbox.add_css_class("boxed-list")
+        content_listbox.set_margin_top(16)
+        content_listbox.set_margin_bottom(16)
+        content_listbox.set_margin_start(16)
+        content_listbox.set_margin_end(16)
+
+        icon_data = find_xsi_icons(icon_dir, search_pattern, False)
+
+        for item in icon_data:
+            item_row = Adw.ActionRow()
+            item_row.set_title(item["FileName"])
+            item_row.set_subtitle(item["FilePath"])
+
+            item_image = Gtk.Image.new_from_icon_name(item["IconName"])
+            item_image.set_pixel_size(64)
+
+            item_row.add_prefix(item_image)
+            #item_row.add_suffix(item_image)
+
+            content_listbox.append(item_row)
+
+        scroll_control.set_child(content_listbox)
+
         # Add widgets to main container
         self.main_content_box.append(app_headerbar)
+        self.main_content_box.append(scroll_control)
 
 
 class MyApp(Adw.Application):
