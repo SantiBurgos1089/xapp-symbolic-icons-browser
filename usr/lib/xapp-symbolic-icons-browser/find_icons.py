3c0dd74e7c954d64212579caaf32e61c10fd8d82
Santiago Burgos <santiago.burgos1089@gmail.com>
2026-03-06 11:54:58 -0600

    created function find_icons to show all XApp symbolic icons available, also defined a test section to show the information needed

---
+++ usr/lib/xapp-symbolic-icons-browser/find_icons.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+import json
+import pathlib
+
+def find_icons(directory, pattern_data = "xsi-*", test=False):
+    base_directory = pathlib.Path(directory)
+    result_info = []
+
+    for id_num, file in enumerate(base_directory.rglob(pattern_data), start=1):
+        if file.is_file():
+            result_info.append({
+                "ID": id_num,
+                "IconName": file.stem,
+                "FileName": file.name,
+                "FilePath": str(file)
+            })
+
+            # Test: get only the first 50 elements if test=True
+            if test is True:
+                if id_num >= 50:
+                    break
+
+    return result_info
+
+## Test section: print the information in console
+## Comment in case you don't need to show information
+#final_data_info = find_icons("/usr/share/icons", "xsi-*", False)
+#json_info = json.dumps(final_data_info, indent=4)
+#print(json_info)
+
